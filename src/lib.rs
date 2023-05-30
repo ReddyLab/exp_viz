817be532f528a6403b604c31a0e7b1ead537b0f8
Thomas Cowart <thomas.cowart@duke.edu>
2023-05-30 16:27:15 -0400

    Expose FilteredBucket and FilteredChromosome

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,7 @@ mod filter_data_structures;
 mod merge;
 
 pub use crate::filter::filter_coverage_data;
-pub use crate::filter_data_structures::{Filter, FilterIntervals, FilteredData};
+pub use crate::filter_data_structures::{
+    BucketList, Filter, FilterIntervals, FilteredBucket, FilteredChromosome, FilteredData,
+};
 pub use crate::merge::merge_filtered_data;
